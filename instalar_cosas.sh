a3802f6b78e48bdf9715fec70f4cd8a5796ea7d9
danduckk <daniselos3@gmail.com>
2026-05-10 22:10:56 +0200

    instalar cosas

---
+++ instalar_cosas.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# install.sh - Instala python, git y clona el repo (Ubuntu)
+
+sudo apt update
+sudo apt install -y python3 git
+git clone https://github.com/danduckk/buscador-ejercicios.git
