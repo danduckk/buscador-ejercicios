b65d333e3c00087ee71bf15ca0dd1f0aa8b649d3
danduckk <daniselos3@gmail.com>
2026-05-10 22:17:29 +0200

    actualización instalar_cosas.sh

--- instalar_cosas.sh
+++ instalar_cosas.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# install.sh - Instala python, git y clona el repo (Ubuntu)
-
 sudo apt update
-sudo apt install -y python3 git
+sudo apt install -y python3 python3-venv python3-pip git
 git clone https://github.com/danduckk/buscador-ejercicios.git
+cd buscador-ejercicios
+python3 -m venv .venv
+.venv/bin/pip install -r requirements.txt
+echo "Listo. Para usar: cd buscador-ejercicios && ./run.sh"
